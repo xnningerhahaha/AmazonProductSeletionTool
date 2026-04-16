09797fd268550f500216178753528ecef9a95c07
肖宁 <shirley.xiao@pkfare.com>
2026-04-16 20:58:03 +0800

    chore: update fix-imports script

--- fix-imports-v2.sh
+++ fix-imports-v2.sh
@@ -6,10 +6,10 @@ cd ~/AmazonProductSeletionTool/backend/dist
 # Fix imports that don't already have .js extension
 # Match patterns like: from './module' or from '../module' but not from './module.js'
 find . -name "*.js" -type f -exec sed -i \
-  -e "s|from '\./\([^']*\)['\"]|from './\1.js'|g" \
-  -e 's|from "\.\/\([^"]*\)"|from "./\1.js"|g' \
-  -e "s|from '\.\./\([^']*\)['\"]|from '../\1.js'|g" \
-  -e 's|from "\.\.\/\([^"]*\)"|from "../\1.js"|g' \
+  -e "s|from '\./\([^']*\)'\$|from './\1.js'|g" \
+  -e 's|from "\.\/\([^"]*\)"$|from "./\1.js"|g' \
+  -e "s|from '\.\./\([^']*\)'\$|from '../\1.js'|g" \
+  -e 's|from "\.\.\/\([^"]*\)"$|from "../\1.js"|g' \
   {} \;
 
 # Fix any double .js.js that might have been created
